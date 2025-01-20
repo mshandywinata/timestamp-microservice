ab467768be8d9ac16e572a596807ea1c14607978
mshandywinata <mshandywinata@upi.edu>
2025-01-20 21:26:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Timestamp Microservice 
+
+It's a project for timestamp microservice challange from freeCodeCamp Backend Development and APIs certification.
+
+## How to run?
+```
+npm install
+npm run start
+```
+
+## Some examples:
+* `{"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"}`
+* `{"error" : "Invalid Date" }`
---
+++ index.js
@@ -0,0 +1,60 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/:date?', function (req, res) {
+  const filter = (str) => {/^\d+$/.test(str)}
+
+  let dateUTC, dateUNIX
+  
+  if (!req.params.date) {
+    dateUTC = new Date()
+    dateUNIX = Math.floor(dateUTC.getTime())
+  } else if (filter(req.params.date)) {
+    dateUNIX = Number(req.params.date)
+    dateUTC = new Date(dateUNIX)
+  } else {
+    const date = new Date(req.params.date)
+
+    if (isNaN(date.getTime())) {
+      res.json({
+        error: "Invalid date"
+      })
+    } else {
+      dateUNIX = date.getTime()
+      dateUTC = date
+    }
+  }
+
+  res.json({
+    "unix": dateUNIX,
+    "utc": dateUTC.toUTCString()
+  })
+})
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
